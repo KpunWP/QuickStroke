270daaca5068b07ceb961ce6014bb991428fda24
KpunWP <kpunkfang@gmail.com>
2026-09-25 11:05:28 +0700

    chore(build): identify Thai TTS test release

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.QS_CONFIG = {
   appName: "QuickStroke",
   version: "1.0.21",
-  buildId: "20260811-clinic-feasibility-result-mode-recovery-v6",
+  buildId: "20260925-jssf-th-tts-pronunciation-v6",
   configVersion: "quickstroke-config-1.0.21",
 
   defaultLang: "th",
