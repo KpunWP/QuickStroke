6da8b3f6e51a1b8ad784bb87f24ade19db128b3d
KpunWP <kpunkfang@gmail.com>
2026-09-26 12:10:57 +0700

    chore(pwa): refresh cache for 90-day consent copy and disabled config

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
  * - JS / JSON / รูปภาพ: stale-while-revalidate
  */
 
-const CACHE_NAME = "quickstroke-pwa-v42";
+const CACHE_NAME = "quickstroke-pwa-v43";
 const CACHE_PREFIX = "quickstroke-pwa-";
 
 const CORE_SHELL = [
